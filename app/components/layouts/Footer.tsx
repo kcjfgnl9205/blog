8a93989a7bf65c35ece76ba3fc4fdbaeaf69a039
kcjfgnl9205 <kcjfgnl9205@gmail.com>
2024-01-22 13:35:27 +0900

    🐛 fix: Footer 위치 수정
    - 오른쪽 정렬 -> 가운데 정렬 수정

--- app/components/layouts/Footer.tsx
+++ app/components/layouts/Footer.tsx
@@ -5,13 +5,11 @@ import { AiFillGithub } from 'react-icons/ai'
 
 export default function Footer() {
   return (
-    <footer className='flex flex-col items-end border-t border-gray-color w-full p-4 dark:border-gray-color'>
-      <div className='flex gap-4'>
-        <Link href='https://github.com/kcjfgnl9205' target='_blank'>
-          <AiFillGithub size={24} className='cursor-pointer' />
-        </Link>
-      </div>
+    <footer className='flex gap-4 justify-center items-center border-t border-gray-color w-full p-4 dark:border-gray-color'>
       <div className='dark:text-gray-color text-sm lg:text-base'>Copyright ©2024 kcjfgnl9205 All rights reserved.</div>
+      <Link href='https://github.com/kcjfgnl9205' target='_blank'>
+        <AiFillGithub size={20} className='cursor-pointer' />
+      </Link>
     </footer>
   )
 }
